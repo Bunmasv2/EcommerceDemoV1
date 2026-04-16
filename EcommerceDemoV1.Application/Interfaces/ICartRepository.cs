df9e818e0994f100b9f06fad3cdd2ae655cb0ab2
bunmasv2 <thaibao5335@gmail.com>
2026-04-16 17:32:58 +0700

    feat(cart): bổ sung ICartRepository và xóa bỏ IJwtRepository cũ

---
+++ EcommerceDemoV1.Application/Interfaces/ICartRepository.cs
@@ -0,0 +1,12 @@
+using EcommerceDemoV1.Domain.Entities;
+
+public interface ICartRepository
+{
+    Task<bool> CartExistsAsync(int cartId);
+    Task<Cart> GetCartByUserIdAsync(int userId);
+    Task<Cart> CreateCartAsync(Cart cart);
+    Task UpdateCartAsync(Cart cart);
+    Task DeleteCartAsync(int cartId);
+    Task UpdateCartItem(CartItem cartItem);
+    Task AddCartItemAsync(CartItem cartItem);
+}
--- EcommerceDemoV1.Application/Interfaces/IJwtRepository.cs
+++
@@ -1,6 +0,0 @@
-using EcommerceDemoV1.Domain.Entities;
-
-public interface IJwtRepository
-{
-    string GenerateToken(int userId, string email, string role);
-}
